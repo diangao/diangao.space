0e32a0966b54ff1729fcd4b12866b03cd39af537
Dian Gao <diangao@umich.edu>
2024-11-13 15:26:26 -0500

    Removed Versel icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
   metadataBase: new URL("https://diangao.space"),
 };
 
-export default function RootLayout({
+export default function RootLayout({ 
   children,
 }: {
   children: React.ReactNode;
